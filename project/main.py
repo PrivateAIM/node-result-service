e8260b83dc2e733dfc03f5d744242ac2247fe689
Maximilian Jugl <Maximilian.Jugl@medizin.uni-leipzig.de>
2024-01-30 11:30:18 +0100

    feat: add `port` parameter to `server` command

--- project/main.py
+++ project/main.py
@@ -13,8 +13,18 @@ def cli():
     default=False,
     help="Enable hot reloading.",
 )
-def server(reload: bool):
-    uvicorn.run("project.server:app", host="0.0.0.0", port=8080, reload=reload)
+@click.option(
+    "--port",
+    "-p",
+    type=int,
+    default=8000,
+    help="Port for the server to listen on.",
+)
+def server(
+    reload: bool,
+    port: int,
+):
+    uvicorn.run("project.server:app", host="0.0.0.0", port=port, reload=reload)
 
 
 if __name__ == "__main__":

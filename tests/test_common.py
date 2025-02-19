ca5fe74892b2c9783971b5ce87af88f43ead8b8a
Maximilian Jugl <Maximilian.Jugl@medizin.uni-leipzig.de>
2025-02-19 09:41:00 +0100

    feat: remove hex conversion of public keys from common module

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,22 +1,8 @@
-from project.common import build_url, hex_to_ecdh_public_key
-from tests.common.helpers import next_ecdh_keypair_bytes
-
-from cryptography.hazmat.primitives import serialization
+from project.common import build_url
 
 
 def test_build_url():
     assert (
         build_url("http", "privateaim.de", "analysis", {"foo": "bar"}, "baz")
         == "http://privateaim.de/analysis?foo=bar#baz"
-    )
-
-
-def test_hex_to_ecdh_public_key():
-    _, pk_bytes = next_ecdh_keypair_bytes()
-    pk_hex = pk_bytes.hex()
-    pk = hex_to_ecdh_public_key(pk_hex)
-
-    assert pk_bytes == pk.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo,
     )

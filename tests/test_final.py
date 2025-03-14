50914de7f7b2f9beb9432dcf344216b52ba99b4b
Mehmed <mehmed.h.93@googlemail.com>
2025-03-14 11:35:55 +0100

    feat: add endpoint for uploading results with local DP

--- tests/test_final.py
+++ tests/test_final.py
@@ -10,6 +10,46 @@ from tests.common.rest import wrap_bytes_for_request, detail_of
 pytestmark = pytest.mark.live
 
 
+def test_200_submit_with_local_dp(test_client, rng, core_client, storage_client, analysis_id):
+    def _check_result_bucket_exists():
+        return core_client.find_analysis_buckets(filter={"analysis_id": analysis_id, "type": "RESULT"})
+
+    assert eventually(_check_result_bucket_exists)
+
+    # Send a valid numerical file
+    raw_value = 5.0
+    blob = str(raw_value).encode("utf-8")
+    filename = "test_result.txt"
+
+    # Set parameters for DP
+    form_data = {
+        "epsilon": "1.0",
+        "sensitivity": "1.0"
+    }
+
+    r = test_client.put(
+        "/final/localdp",
+        auth=BearerAuth(issue_client_access_token(analysis_id)),
+        files={"file": (filename, blob, "text/plain")},
+        data=form_data,
+    )
+
+    assert r.status_code == status.HTTP_204_NO_CONTENT, f"Unexpected status code: {r.status_code}"
+
+    # retrieve result and see if it returned a file with single number
+    uploaded_files = core_client.find_analysis_bucket_files(
+        filter={"analysis_id": analysis_id, "type": "RESULT"}
+    )
+
+    if uploaded_files:
+        stored_file = uploaded_files[-1]  # Get the most recent file
+        stored_content = b''.join(storage_client.stream_bucket_file(stored_file.external_id))
+
+        if stored_content:
+            noisy_value = float(stored_content.decode("utf-8"))
+            assert noisy_value != raw_value, "Noisy value should be different from raw value!"
+
+
 def test_200_submit_to_upload(test_client, rng, core_client, analysis_id):
     def _check_result_bucket_exists():
         return core_client.find_analysis_buckets(filter={"analysis_id": analysis_id, "type": "RESULT"})

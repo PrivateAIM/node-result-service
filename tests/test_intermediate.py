4c140fce31c5648add9d0b90f04145f7393b9e37
pbrassel <52356233+pbrassel@users.noreply.github.com>
2025-07-08 14:41:34 +0200

    test: refactor tests for the intermediate endpoint

--- tests/test_intermediate.py
+++ tests/test_intermediate.py
@@ -1,9 +1,10 @@
 import uuid
 
+from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat
 import pytest
 from starlette import status
 
-from project import crypto
+from project.crypto import decrypt_default, load_ecdh_private_key
 from project.routers.intermediate import IntermediateUploadResponse
 from tests.common.auth import (
     BearerAuth,
@@ -12,7 +13,6 @@ from tests.common.auth import (
 )
 from tests.common.helpers import (
     next_random_bytes,
-    eventually,
     next_uuid,
     next_ecdh_keypair_bytes,
 )
@@ -22,86 +22,35 @@ pytestmark = pytest.mark.live
 
 
 @pytest.fixture()
-def node(robot_auth_client, core_client, realm_id):
-    node_name = next_uuid()
-    new_node = core_client.create_node(name=node_name, realm_id=realm_id, node_type="default")
-    yield new_node
-    core_client.delete_node(new_node.id)
+def node(core_client, realm_id):
+    node = core_client.create_node(name=next_uuid(), realm_id=realm_id, node_type="default")
+    yield node
+    core_client.delete_node(node.id)
 
 
-def test_200_submit_receive_intermediate_encrypted(test_client, rng, analysis_id, node, core_client):
-    def _check_temp_bucket_exists():
-        return len(core_client.find_analysis_buckets(filter={"analysis_id": analysis_id, "type": "TEMP"})) == 1
-
-    assert eventually(_check_temp_bucket_exists)
-
-    private_key, public_key = get_test_ecdh_keypair()
-
-    # update node with public key
-    remote_private_key_bytes, remote_public_key_bytes = next_ecdh_keypair_bytes()
-    core_client.update_node(node, public_key=remote_public_key_bytes.decode("ascii"))
-
-    # update local node reference (public key must be updated)
-    node = core_client.get_node(node.id)
-    assert node.public_key is not None
-
-    blob = next_random_bytes(rng)
-    r = test_client.put(
-        "/intermediate",
-        auth=BearerAuth(issue_client_access_token(analysis_id)),
-        files=wrap_bytes_for_request(blob),
-        data={"remote_node_id": str(node.id)},
+@pytest.fixture()
+def this_node(core_client, realm_id):
+    node = core_client.create_node(name=next_uuid(), realm_id=realm_id, node_type="default")
+    _, public_key = get_test_ecdh_keypair()
+    # Also update node reference.
+    node = core_client.update_node(
+        node, public_key=public_key.public_bytes(Encoding.PEM, PublicFormat.SubjectPublicKeyInfo).decode("ascii")
     )
-
-    assert r.status_code == status.HTTP_200_OK
-
-    # check that the response contains a path to a valid resource
-    model = IntermediateUploadResponse(**r.json())
-    assert str(model.object_id) in str(model.url.path)
-
-    r = test_client.get(
-        model.url.path,
-        auth=BearerAuth(issue_client_access_token()),
-    )
-
-    assert r.status_code == status.HTTP_200_OK
-
-    # read encrypted blob
-    ct_blob = r.read()
-    assert ct_blob != blob
-
-    # pretend we're the remote node and decrypt using their private key
-    decrypted_blob = crypto.decrypt_default(crypto.load_ecdh_private_key(remote_private_key_bytes), public_key, ct_blob)
-
-    assert blob == decrypted_blob
+    yield node
+    core_client.delete_node(node.id)
 
 
-def test_400_submit_encrypted_no_remote_public_key(test_client, rng, analysis_id, node, core_client):
-    def _check_temp_bucket_exists():
-        return len(core_client.find_analysis_buckets(filter={"analysis_id": analysis_id, "type": "TEMP"})) == 1
-
-    assert eventually(_check_temp_bucket_exists)
-
-    blob = next_random_bytes(rng)
-    r = test_client.put(
-        "/intermediate",
-        auth=BearerAuth(issue_client_access_token(analysis_id)),
-        files=wrap_bytes_for_request(blob),
-        data={
-            "remote_node_id": str(node.id),
-        },
-    )
-
-    assert r.status_code == status.HTTP_400_BAD_REQUEST
-    assert detail_of(r) == f"Remote node with ID {node.id} does not provide a public key"
+@pytest.fixture()
+def remote_node_and_private_key(core_client, realm_id):
+    node = core_client.create_node(name=next_uuid(), realm_id=realm_id, node_type="default")
+    private_key, public_key = next_ecdh_keypair_bytes()
+    # Also update node reference.
+    node = core_client.update_node(node, public_key=public_key.decode("ascii"))
+    yield node, private_key
+    core_client.delete_node(node.id)
 
 
-def test_200_submit_receive_intermediate(test_client, rng, analysis_id, core_client):
-    def _check_temp_bucket_exists():
-        return len(core_client.find_analysis_buckets(filter={"analysis_id": analysis_id, "type": "TEMP"})) == 1
-
-    assert eventually(_check_temp_bucket_exists)
-
+def test_200_submit_receive_intermediate(test_client, rng, analysis_id, check_buckets_exist, core_client):
     blob = next_random_bytes(rng)
     r = test_client.put(
         "/intermediate",
@@ -111,7 +60,7 @@ def test_200_submit_receive_intermediate(test_client, rng, analysis_id, core_cli
 
     assert r.status_code == status.HTTP_200_OK
 
-    # check that the response contains a path to a valid resource
+    # Check that the response contains a path to a valid resource.
     model = IntermediateUploadResponse(**r.json())
     assert str(model.object_id) in str(model.url.path)
 
@@ -122,6 +71,50 @@ def test_200_submit_receive_intermediate(test_client, rng, analysis_id, core_cli
 
     assert r.status_code == status.HTTP_200_OK
     assert r.read() == blob
+
+
+def test_200_submit_receive_intermediate_encrypted(
+    test_client, core_client, rng, analysis_id, check_buckets_exist, remote_node_and_private_key
+):
+    remote_node, remote_private_key = remote_node_and_private_key
+    blob = next_random_bytes(rng)
+    r = test_client.put(
+        "/intermediate",
+        auth=BearerAuth(issue_client_access_token(analysis_id)),
+        files=wrap_bytes_for_request(blob),
+        data={
+            "remote_node_id": str(remote_node.id),
+        },
+    )
+
+    assert r.status_code == status.HTTP_200_OK
+
+    model = IntermediateUploadResponse(**r.json())
+
+    r = test_client.get(
+        model.url.path,
+        auth=BearerAuth(issue_client_access_token()),
+    )
+
+    _, public_key = get_test_ecdh_keypair()
+
+    assert blob == decrypt_default(load_ecdh_private_key(remote_private_key), public_key, r.read())
+
+
+def test_400_submit_encrypted_no_remote_public_key(
+    test_client, rng, analysis_id, check_buckets_exist, node, core_client
+):
+    r = test_client.put(
+        "/intermediate",
+        auth=BearerAuth(issue_client_access_token(analysis_id)),
+        files=wrap_bytes_for_request(next_random_bytes(rng)),
+        data={
+            "remote_node_id": str(node.id),
+        },
+    )
+
+    assert r.status_code == status.HTTP_400_BAD_REQUEST
+    assert detail_of(r) == f"Remote node with ID {node.id} does not provide a public key"
 
 
 def test_404_invalid_id(test_client):
@@ -135,15 +128,72 @@ def test_404_invalid_id(test_client):
     assert detail_of(r) == f"Object with ID {rand_uuid} does not exist"
 
 
+def test_404_no_remote_node(test_client, analysis_id, check_buckets_exist, core_client, rng):
+    rand_uuid = str(uuid.uuid4())
+    r = test_client.put(
+        "/intermediate",
+        auth=BearerAuth(issue_client_access_token(analysis_id)),
+        files=wrap_bytes_for_request(next_random_bytes(rng)),
+        data={
+            "remote_node_id": rand_uuid,
+        },
+    )
+
+    assert r.status_code == status.HTTP_404_NOT_FOUND
+    assert detail_of(r) == f"Remote node with ID {rand_uuid} does not exist."
+
+
 def test_404_submit_invalid_id(test_client, rng):
     rand_uuid = str(uuid.uuid4())
-    blob = next_random_bytes(rng)
 
     r = test_client.put(
         "/intermediate",
         auth=BearerAuth(issue_client_access_token(rand_uuid)),
-        files=wrap_bytes_for_request(blob),
+        files=wrap_bytes_for_request(next_random_bytes(rng)),
     )
 
     assert r.status_code == status.HTTP_404_NOT_FOUND
     assert detail_of(r) == f"Temp bucket for analysis with ID {rand_uuid} was not found"
+
+
+def test_400_decrypt_intermediate(
+    test_client,
+    core_client,
+    analysis_id,
+    check_buckets_exist,
+    this_node,
+    remote_node_and_private_key,
+    node,
+    rng,
+    realm_id,
+):
+    blob = next_random_bytes(rng)
+    r = test_client.put(
+        "/intermediate",
+        auth=BearerAuth(issue_client_access_token(analysis_id)),
+        files=wrap_bytes_for_request(blob),
+        data={
+            "remote_node_id": str(remote_node_and_private_key[0].id),
+        },
+    )
+
+    assert r.status_code == status.HTTP_200_OK
+
+    # Check that the response contains a path to a valid resource.
+    model = IntermediateUploadResponse(**r.json())
+    assert str(model.object_id) in str(model.url.path)
+
+    _, arbitrary_public_key = next_ecdh_keypair_bytes()
+    core_client.update_node(node.id, public_key=arbitrary_public_key.decode("ascii"))
+
+    r = test_client.get(
+        model.url.path,
+        auth=BearerAuth(issue_client_access_token()),
+        params={"node_id": node.id},
+    )
+
+    assert r.status_code == status.HTTP_400_BAD_REQUEST
+    assert detail_of(r) == (
+        f"File with ID {model.object_id} cannot be decrypted under the assumption that the file was encrypted by node "
+        f"{node.id} for this node."
+    )

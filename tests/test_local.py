6747f92898fc793920e92cb6e70a71d56e69e558
pbrassel <52356233+pbrassel@users.noreply.github.com>
2025-09-25 16:35:06 +0200

    fix: project-wide accessible local results
    Analyses were not able to access local results of analyses that belong to the same project. Local results used to be grouped by their `analysis_id`. For simplicity they are now grouped by their `project_id` so that accessing results from other project-related analyses is less complex.

--- tests/test_local.py
+++ tests/test_local.py
@@ -6,15 +6,20 @@ from project.routers.local import (
     LocalUploadResponse,
 )
 from tests.common.auth import BearerAuth, issue_client_access_token
-from tests.common.helpers import next_random_bytes
+from tests.common.helpers import next_random_bytes, eventually
 from tests.common.rest import wrap_bytes_for_request, detail_of
 
 
-def test_200_submit_receive_from_local(test_client, rng):
+def test_200_submit_receive_from_local(test_client, rng, core_client, analysis_id):
+    def _analysis_exists():
+        return core_client.get_analysis(analysis_id) is not None
+
+    assert eventually(_analysis_exists)
+
     blob = next_random_bytes(rng)
     r = test_client.put(
         "/local",
-        auth=BearerAuth(issue_client_access_token()),
+        auth=BearerAuth(issue_client_access_token(analysis_id)),
         files=wrap_bytes_for_request(blob),
     )
 
@@ -23,18 +28,23 @@ def test_200_submit_receive_from_local(test_client, rng):
 
     r = test_client.get(
         model.url.path,
-        auth=BearerAuth(issue_client_access_token()),
+        auth=BearerAuth(issue_client_access_token(analysis_id)),
     )
 
     assert r.status_code == status.HTTP_200_OK
     assert r.read() == blob
 
 
-def test_404_unknown_oid(test_client):
+def test_404_unknown_oid(test_client, core_client, analysis_id):
+    def _analysis_exists():
+        return core_client.get_analysis(analysis_id) is not None
+
+    assert eventually(_analysis_exists)
+
     oid = uuid.uuid4()
     r = test_client.get(
         f"/local/{oid}",
-        auth=BearerAuth(issue_client_access_token()),
+        auth=BearerAuth(issue_client_access_token(analysis_id)),
     )
 
     assert r.status_code == status.HTTP_404_NOT_FOUND

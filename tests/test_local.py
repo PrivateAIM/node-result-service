8d79b505b2161d1778af9df0edb97f6272ff4aa6
pbrassel <52356233+pbrassel@users.noreply.github.com>
2025-09-25 16:37:22 +0200

    test: local results are only accessible from analyses that belong to the same project

--- tests/test_local.py
+++ tests/test_local.py
@@ -49,3 +49,47 @@ def test_404_unknown_oid(test_client, core_client, analysis_id):
 
     assert r.status_code == status.HTTP_404_NOT_FOUND
     assert detail_of(r) == f"Object with ID {oid} does not exist"
+
+
+def test_200_result_from_another_analysis(test_client, core_client, analysis_id_factory, rng):
+    first_analysis_id, second_analysis_id = analysis_id_factory(), analysis_id_factory()
+
+    def _analyses_exist():
+        return core_client.get_analysis(first_analysis_id) is not None and core_client.get_analysis(second_analysis_id)
+
+    eventually(_analyses_exist)
+
+    blob = next_random_bytes(rng)
+    r = test_client.put(
+        "/local",
+        auth=BearerAuth(issue_client_access_token(first_analysis_id)),
+        files=wrap_bytes_for_request(blob),
+    )
+
+    r = test_client.get(r.json()["url"], auth=BearerAuth(issue_client_access_token(second_analysis_id)))
+
+    assert r.status_code == status.HTTP_200_OK
+    assert r.read() == blob
+
+
+def test_404_result_from_another_project(test_client, core_client, rng, project_id_factory, analysis_id_factory):
+    first_analysis_id = analysis_id_factory(project_id_factory())
+    second_analysis_id = analysis_id_factory(project_id_factory())
+
+    def _analyses_exist():
+        return core_client.get_analysis(first_analysis_id) is not None and core_client.get_analysis(second_analysis_id)
+
+    eventually(_analyses_exist)
+
+    blob = next_random_bytes(rng)
+    r = test_client.put(
+        "/local",
+        auth=BearerAuth(issue_client_access_token(first_analysis_id)),
+        files=wrap_bytes_for_request(blob),
+    )
+
+    object_id = r.json()["url"].split("/")[-1]
+    r = test_client.get(r.json()["url"], auth=BearerAuth(issue_client_access_token(second_analysis_id)))
+
+    assert r.status_code == status.HTTP_404_NOT_FOUND
+    assert detail_of(r) == f"Object with ID {object_id} does not exist"
